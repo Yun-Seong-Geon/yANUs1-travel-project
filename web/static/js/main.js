10b73a99356290932a39210d9b9270cc1348336a
dudnami <dudnami.gmail.com>
2023-08-07 17:04:38 +0900

    프론트 메인수정2

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,3 +1,20 @@
+function handleKeyPress(event) {
+  // Enter 키의 keyCode는 13
+  if (event.keyCode === 13) {
+      var inputText = document.getElementById("inputText").value;{
+      transBox();}
+       alert("입력한 텍스트: " + inputText);
+  }
+}
+function transBox() {
+  var box = document.getElementById('transBox');
+  if (box.style.display === 'none') {
+    box.style.display = 'block';
+  } else {
+    box.style.display = 'none';
+  }
+}
+
 function checkInput() {
     var text = document.getElementById("trans_text").value;
     if (text == "") { //해당 입력값이 없을 경우 같은말: if(!trans_text.value)
