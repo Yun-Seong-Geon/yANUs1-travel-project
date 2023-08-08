3d5480908a8916a2206d05141cb9a076b8f5edac
YUN <canyun6676@gmail.com>
2023-08-08 13:27:45 +0900

    create_app 위치변경

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template
 import random
 from web import create_app
 
-app = create_app()
+
+
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True,port=5001)
