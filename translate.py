24e707bffbe13c2c2fd562fe5453374320505b86
YUN <canyun6676@gmail.com>
2023-08-23 14:01:13 +0900

    번역기 업데이트

--- translate.py
+++ translate.py
@@ -4,11 +4,11 @@
 from googletrans import Translator
 from google.cloud import translate_v2 as translate
 from google.oauth2.credentials import Credentials
+from google.auth.exceptions import DefaultCredentialsError
 
 class GoogleCloudTranslator:
     def __init__(self, credentials_path: str):
-        creds = Credentials.from_authorized_user_file(credentials_path)
-        self.client = translate.Client(credentials=creds)
+        self.client = translate.Client.from_service_account_json(credentials_path)
         self.result = {'src_text': '', 'src_lang': '', 'tgt_text': '', 'tgt_lang': ''}
 
     def translate(self, text: str, lang='en') -> dict:
@@ -20,53 +20,10 @@ class GoogleCloudTranslator:
         self.result['tgt_lang'] = lang
 
         return self.result
-
-# 사용 예시:
-
-
-
+ ## 사용 예시   
+if __name__ == '__main__':
+    translator = GoogleCloudTranslator('abstract-plane-396801-904742608cb2.json')
+    test_to_translate = input('입력해라 : ')
+    translated_text = translator.translate(test_to_translate, 'en')['tgt_text']
+    print(translated_text)
     
-    
-    ## 파이썬 번역 프로그램 
-# if __name__ == '__main__':
-#     translator = Google_Translator()
-#     print(translate_eng('안녕하세요!!'))
- 
-#     # Select the language you want to translate to
-#     tgt_lang_code = input('# Enter language code (Default: en): ')
- 
-#     if tgt_lang_code == '':
-#         tgt_lang_code = 'en'
- 
-#     print('>> You chose: {}\n'.format(tgt_lang_code))
- 
-#     # Select the option you want to use
-#     input_message = '# Pick an option: text or file: \n'
- 
-#     for index, option in enumerate(options):
-#         input_message += f'{index + 1}. {option}\n'
-    
-#     input_message += 'Enter your choice: '
- 
-#     option = input(input_message)
-    
-#     print('>> You chose: {}\n'.format(options[int(option) - 1]))
- 
-#     # Translate the text
-#     if option == '1':
-#         input_text = input('Press Enter to translate: ')
-#         result = translator.translate(input_text, tgt_lang_code)
- 
-#         print('[{}] -> [{}]'.format(result['src_lang'], result['tgt_lang']))
-#         print('=' * 50)
-#         print('Source Text : {}'.format(result['src_text']))
-#         print('Target Text : {}'.format(result['tgt_text']))
-#     # Translate the file
-#     elif option == '2':
-#         file_path = input('Enter file path: ')
-#         result = translator.translate_file(file_path, tgt_lang_code)
- 
-#         print('[{}] -> [{}]'.format(result['src_lang'], result['tgt_lang']))
-#         print('=' * 50)
-#         print('Source Text : [{}]\n'.format(result['src_text']))
-#         print('Target Text : [{}]'.format(result['tgt_text']))
